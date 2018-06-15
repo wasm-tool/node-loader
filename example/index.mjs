96fb252b7c6f6fb12386d91e503a17821ddc0f42
Sven SAULEAU <sven.sauleau@xtuc.fr>
2018-06-15 16:26:03 +0200

    feat: enable ESM dependecies

--- example/index.mjs
+++ example/index.mjs
@@ -1,3 +1,25 @@
+// (module
+//   (type (func (param i32) (param i32) (result i32)))
+//   (func $addTwo (param i32) (param i32) (result i32)
+//     (get_local 0)
+//     (get_local 1)
+//     (i32.add)
+//   )
+//   (export "addTwo" (func $addTwo))
+// )
 import {addTwo} from "./addTwo.wasm";
 
+
+// (module
+//   (type (func (result i32)))
+//   (import "./env.mjs" "n" (global i32))
+//   (func (result i32)
+//     (get_global 0)
+//   )
+//   (export "get" (func 0))
+// )
+import {get} from "./get.wasm";
+
 console.log("addTwo", addTwo(1, 2));
+
+console.log("get", get());
--- loader.mjs
+++ loader.mjs
@@ -1,6 +1,24 @@
 import fs from "fs"
 import path from "path"
 
+import parser from "@webassemblyjs/wasm-parser";
+import t from "@webassemblyjs/ast";
+
+let internalDefaultResolver;
+
+const decoderOpts = {
+  ignoreCodeSection: true,
+  ignoreDataSection: true,
+  ignoreCustomNameSection: true
+};
+
+async function loadDependency(module, name) {
+  const fqmodule = internalDefaultResolver(module).url;
+  const dep = await import(fqmodule);
+
+  return dep[name];
+}
+
 function readFile(path) {
   return fs.readFileSync(path, null);
 }
@@ -9,20 +27,59 @@ export async function dynamicInstantiate(url) {
   const buffer = readFile(url);
   const module = new WebAssembly.Module(buffer);
 
-  const wasmExports = WebAssembly.Module.exports(module);
-  const wasmInstance = new WebAssembly.Instance(module);
+  const wasmExports = [];
+  const wasmImports = [];
+  const dependenciesPromise = [];
+
+  /**
+   * wasm introspection
+   */
+  const ast = parser.decode(buffer, decoderOpts);
+
+  t.traverse(ast, {
+    ModuleExport({ node }) {
+      wasmExports.push(node.name);
+    },
+
+    ModuleImport({ node }) {
+      dependenciesPromise.push(
+        loadDependency(node.module, node.name)
+      );
+
+      wasmImports.push([node.module, node.name]);
+    }
+  });
+
+  /**
+   * generate importObject
+   */
+  const importObject = {};
+
+  const dependencies = await Promise.all(dependenciesPromise);
+
+  wasmImports.forEach(([module, name], i) => {
+    if (typeof importObject[module] === "undefined") {
+      importObject[module] = {};
+    }
+
+    importObject[module][name] = dependencies[i];
+  });
+
+  /**
+   * instantiation
+   */
+  const wasmInstance = new WebAssembly.Instance(module, importObject);
 
   return {
-    exports: wasmExports.map(({name}) => name),
+    exports: wasmExports,
     execute: exports => {
 
-      wasmExports.forEach(({name}) => {
+      wasmExports.forEach(name => {
         exports[name].set(wasmInstance.exports[name]);
       });
     }
   };
 }
-
 
 export function resolve(specifier, base, defaultResolver) {
   const ext = path.extname(specifier);
@@ -33,6 +90,8 @@ export function resolve(specifier, base, defaultResolver) {
       format: 'dynamic'
     };
   }
+
+  internalDefaultResolver = defaultResolver;
 
   return defaultResolver(specifier, base);
 }
