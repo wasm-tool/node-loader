5c1db3034290f2918afd6a3a409f47825da6e8b3
Ingvar Stepanyan <me@rreverser.com>
2019-01-17 21:57:41 +0000

    Simplify loader code

--- loader.mjs
+++ loader.mjs
@@ -18,42 +18,30 @@ export async function dynamicInstantiate(url) {
   const buffer = readFile(new URL(url));
   const module = new WebAssembly.Module(buffer);
 
-  const wasmExports = [];
-  const wasmImports = [];
-  const dependenciesPromise = [];
-
-  WebAssembly.Module.imports(module).forEach((i) => {
-    dependenciesPromise.push(loadDependency(i.module, i.name));
-    wasmImports.push([i.module, i.name]);
-  });
-  WebAssembly.Module.exports(module).forEach(({ name }) => {
-    wasmExports.push(name);
-  });
-
   /**
    * generate importObject
    */
   const importObject = {};
 
-  const dependencies = await Promise.all(dependenciesPromise);
-
-  wasmImports.forEach(([module, name], i) => {
+  await Promise.all(WebAssembly.Module.imports(module).map(async ({ module, name }) => {
     if (typeof importObject[module] === "undefined") {
       importObject[module] = {};
     }
 
-    importObject[module][name] = dependencies[i];
-  });
+    importObject[module][name] = await loadDependency(module, name);
+  }));
 
   /**
    * instantiation
    */
   const wasmInstance = new WebAssembly.Instance(module, importObject);
 
+  const wasmExports = WebAssembly.Module.exports(module).map(({ name }) => name);
+
   return {
     exports: wasmExports,
-    execute: exports => {
 
+    execute: exports => {
       wasmExports.forEach(name => {
         exports[name].set(wasmInstance.exports[name]);
       });
